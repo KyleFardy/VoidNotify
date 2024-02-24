2456f7c8e37a40c18be08b0d50b0dee12e98b41b
Kyle Fardy <fardy662@gmail.com>
2024-02-24 12:44:42 +0000

    .

--- server/update.lua
+++ server/update.lua
@@ -1,14 +1,29 @@
 AddEventHandler('onResourceStart', function(resourceName)
     if Config.CheckForUpdates then
         Citizen.CreateThread(function()
-            resource_name = "Void Notify (" .. GetCurrentResourceName() .. ")"
+            resource_name = GetCurrentResourceName()
             PerformHttpRequest("https://api.github.com/repos/KyleFardy/VoidNotify/releases/latest", check_version, "GET")
         end)
     end
 end)
+function get_version()
+    local file = io.open(GetResourcePath(GetCurrentResourceName()) .. "/fxmanifest.lua", "r")
+    if file then
+        local content = file:read("*all")
+        file:close()
+        local version = content:match("version%s*['\"](%d+%.%d+)['\"]")
+        if version then
+            return version
+        else
+            return "Unknown"
+        end
+    else
+        print("Unable To Open fxmanifest.lua")
+    end
+end
+
 function check_version(err, response, headers)
-    
-    current_version = LoadResourceFile(GetCurrentResourceName(), "version")
+    current_version = get_version()
     if response == nil then
         print("^1" .. resource_name .. " Checking For Update Failed!^7")
         return
@@ -17,17 +32,17 @@ function check_version(err, response, headers)
     if current_version ~= response_obj.tag_name and tonumber(current_version) < tonumber(response_obj.tag_name) then
         print("\n^1----------------------------------------------------------------------------------^7")
         print(resource_name ..
-        " Is Outdated, The Latest Version Is: ^2" ..
-        response_obj.tag_name ..
-        "^7, Installed Version: ^1" ..
-        current_version .. "^7!\nDownload The Update From https://github.com/KyleFardy/VoidNotify")
+            " Is Outdated, The Latest Version Is: ^2" ..
+            response_obj.tag_name ..
+            "^7, Installed Version: ^1" ..
+            current_version .. "^7!\nDownload The Update From https://github.com/KyleFardy/VoidNotify")
         print("^1----------------------------------------------------------------------------------^7")
     elseif tonumber(current_version) > tonumber(response_obj.tag_name) then
         print("\n^3----------------------------------------------------------------------------------^7")
         print(resource_name ..
-        " Github Version Is: ^2" .. response_obj.tag_name .. "^7, Installed Version: ^1" .. current_version .. "^7!")
+            " Github Version Is: ^2" .. response_obj.tag_name .. "^7, Installed Version: ^1" .. current_version .. "^7!")
         print("^3----------------------------------------------------------------------------------^7")
     else
-        print("\n" .. resource_name .. " Is Up To Date! (^2" .. current_version .. "^7)")
+        print(resource_name .. " Is Up To Date! (^2" .. current_version .. "^7)")
     end
 end
